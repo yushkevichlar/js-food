22c97c41ee9eee3ea97bf913facd90c4de374f32
L <yushkevichlar@gmail.com>
2021-05-28 23:11:38 +0300

    tabs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener('DOMContentLoaded', () => {
-
+	
 	const tabs = document.querySelectorAll('.tabheader__item'),
 		  tabsContent = document.querySelectorAll('.tabcontent'),
 		  tabsParent = document.querySelector('.tabheader__items');
